0d5631c5263e7eda74c42cf916c00ccd92bde000
Kadel Lakatt <kicklp@hotmail.com>
2021-01-12 19:35:54 -0500

    Solution and Projects created.

---
+++ BlogifySolution/BlogifyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogifyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
